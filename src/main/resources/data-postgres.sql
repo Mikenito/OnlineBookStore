b74cc5933345faf82ad743fdec1ca6ac0d916ed7
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 13:28:28 +0200

    added a data dump script for db initialization

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,10 @@
+insert into "liquibase-demo".public.books(isbn, author, title)
+values ('9783161484100', 'The Horrors', 'Teddy Martingale'),
+       ('9783161484101', 'Faster', 'John Madea'),
+       ('9783161484103', 'RedBox', 'Hilary Ford'),
+       ('9783161484104', 'Many', 'Josh Fix'),
+       ('9783161484105', '50 Laps', 'Gary Champ'),
+       ('9783161484106', 'Top Man', 'Miles Fern'),
+       ('9783161484107', 'Hidden', 'Courtney Mbalula'),
+       ('9783161484108', 'Jump', 'Alex Mota'),
+       ('9783161484109', 'Addict', 'Alfred Zaine');
