64d677e7b461874f788f109e2f77442bb729f993
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 22:46:23 +0200

    added api for retrieving the book orders

--- src/main/java/com/online/book/store/controllers/OrderController.java
+++ src/main/java/com/online/book/store/controllers/OrderController.java
@@ -1,22 +1,34 @@
 package com.online.book.store.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.online.book.store.domain.Order;
+import com.online.book.store.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 @RequestMapping ( "/api/orders" )
 public class OrderController {
 
+    private OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
     @PostMapping
-    public String createBookOrder() {
-        return "will create book orders";
+    public Order createBookOrder(@RequestBody HashMap<String, String> orders) {
+        String ISBN = orders.get("ISBN");
+        Integer quantity = Integer.parseInt(orders.get("quantity"));
+        return this.orderService.saveBookOrder(ISBN, quantity);
     }
 
     @GetMapping
-    public String getAllBookOrders() {
-        return "will return all book orders";
+    public List<Order> getAllBookOrders() {
+        return this.orderService.getAllBookOrders();
     }
 
 }
