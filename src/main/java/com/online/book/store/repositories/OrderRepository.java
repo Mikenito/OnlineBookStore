e2ad6371ad5155e1eea41876653149356fec3a76
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 12:28:48 +0200

    added the repository interfaces

---
+++ src/main/java/com/online/book/store/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.online.book.store.repositories;
+
+import com.online.book.store.domain.Order;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends CrudRepository<Order, Integer> {
+}
