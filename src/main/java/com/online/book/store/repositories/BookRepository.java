18668942c5ddfa9f5c1d09c5e2d1e30a4e105225
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 15:47:40 +0200

    added a method to retrieve a book using ISBN

--- src/main/java/com/online/book/store/repositories/BookRepository.java
+++ src/main/java/com/online/book/store/repositories/BookRepository.java
@@ -4,6 +4,11 @@ import com.online.book.store.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
+
+    Optional<Book> findBookByISBN(String ISBN);
+
 }
