319249676e1fe965584e095d679ba4eeaafe49a4
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 22:51:22 +0200

    modified the retrieval method

--- src/main/java/com/online/book/store/repositories/BookRepository.java
+++ src/main/java/com/online/book/store/repositories/BookRepository.java
@@ -4,11 +4,9 @@ import com.online.book.store.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
-    Optional<Book> findBookByISBN(String ISBN);
+    Book findBookByISBN(String ISBN);
 
 }
