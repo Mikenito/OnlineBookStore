063ad07a895bd1d21515f879bfb2bab178918e84
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 13:27:48 +0200

    added fields to the entities

--- src/main/java/com/online/book/store/domain/Book.java
+++ src/main/java/com/online/book/store/domain/Book.java
@@ -1,6 +1,33 @@
 package com.online.book.store.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "books")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column (nullable = false, name = "isbn")
+    private String ISBN;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String author;
+
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
 
 }
--- src/main/java/com/online/book/store/domain/Order.java
+++ src/main/java/com/online/book/store/domain/Order.java
@@ -1,5 +1,29 @@
 package com.online.book.store.domain;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table ( name = "orders" )
 public class Order {
+
+    @Id
+    @GeneratedValue ( strategy = GenerationType.AUTO )
+    private int id;
+
+    @Column
+    private int quantity;
+
+    @Column
+    private String ISBN;
+
+    @Column
+    @OneToMany ( cascade = CascadeType.ALL )
+    private List<Book> books;
 
 }
