3314d39df12e8315ffbe6a03a99530d7e44e5086
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 15:46:22 +0200

    added logic for retrieving a book using ISBN

--- src/main/java/com/online/book/store/services/BookService.java
+++ src/main/java/com/online/book/store/services/BookService.java
@@ -1,8 +1,13 @@
 package com.online.book.store.services;
 
+import com.online.book.store.domain.Book;
 import com.online.book.store.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @Service
 public class BookService {
@@ -12,6 +17,14 @@ public class BookService {
     @Autowired
     public BookService(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    public List<Book> findBooksByISBN(Set<String> ISBN){
+        List<Book> books = new ArrayList<>();
+        for (String ISBNs : ISBN) {
+            books.add(this.bookRepository.findBookByISBN(ISBNs).get());
+        }
+        return books;
     }
 
 }
