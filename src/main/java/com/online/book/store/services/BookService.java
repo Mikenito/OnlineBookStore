792a49d50f855d6cf976c7a27686209c24a726e0
Michael Nyembe <michael@psybergate.co.za>
2021-03-09 12:33:35 +0200

    added service for order and book

---
+++ src/main/java/com/online/book/store/services/BookService.java
@@ -0,0 +1,17 @@
+package com.online.book.store.services;
+
+import com.online.book.store.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+}
---
+++ src/main/java/com/online/book/store/services/OrderService.java
@@ -0,0 +1,17 @@
+package com.online.book.store.services;
+
+import com.online.book.store.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+}
